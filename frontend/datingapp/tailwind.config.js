a3fb196af709e364999832800bb85e1a84396bf1
Duy Ly <thebestduyever@gmail.com>
2026-06-26 13:42:40 +0700

    a

---
+++ frontend/datingapp/tailwind.config.js
@@ -0,0 +1,9 @@
+/ @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["./src//*.{html,ts}", "./node_modules/preline/preline.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("preline/plugin")],
+};
